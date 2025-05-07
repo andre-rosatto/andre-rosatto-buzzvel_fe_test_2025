81bdd79d9f9f244a5c2a329659757bec98c5c585
André Rosatto <linus1178@gmail.com>
2025-05-07 20:36:41 -0300

    meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         <title>uteach</title>
         <meta
           name='description'
-          content="Nextjs website created for uteach as part of Buzzvel's front-end technical challenge 2025"
+          content="Nextjs website created as a Single Page Application for the education company uteach as part of Buzzvel's front-end technical challenge 2025"
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
