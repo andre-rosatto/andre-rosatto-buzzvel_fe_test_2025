f4ee446f8685571be0deeccd4a99e5e3dcb7bbaf
André Rosatto <linus1178@gmail.com>
2025-05-07 16:58:30 -0300

    layout fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -223,6 +223,7 @@ export default function Footer() {
                 viewBox='0 0 24 25'
                 fill='none'
                 xmlns='http://www.w3.org/2000/svg'
+                className={styles.currencySymbol}
               >
                 <path
                   d='M18.5 5.30423C17.4428 4.78906 16.2552 4.5 15 4.5C10.5817 4.5 7 8.08172 7 12.5C7 16.9183 10.5817 20.5 15 20.5C16.2552 20.5 17.4428 20.2109 18.5 19.6958'
