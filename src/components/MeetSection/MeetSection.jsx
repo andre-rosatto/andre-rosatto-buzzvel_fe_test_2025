46f09c7c000286c35e4c76b257ee3a269022ea60
André Rosatto <linus1178@gmail.com>
2025-05-07 10:45:23 -0300

    layout fixes

---
+++ src/components/MeetSection/MeetSection.jsx
@@ -0,0 +1,5 @@
+import styles from './MeetSection.module.css';
+
+export default function MeetSection() {
+  return <div>MeetSection</div>;
+}
---
+++ src/components/MeetSection/index.js
@@ -0,0 +1,3 @@
+import MeetSection from "./MeetSection";
+
+export default MeetSection;
